b8c4ab14d4ec81f50163534a0e8830228884273b
kariya <kariya@kariya.cc>
2013-11-01 22:06:19 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# BrainF*ck imlementation for HP50g calculator
+
+## What's this?
+This is a implementation (currently interpreter only) of BrainF*ck language[2] for HP50g calculator.
+It's an ARM native code application, not emulated Saturn code.
+
+
+## Usage
+Copy bin/bfi.hp and data/*.b to HP50g.
+Push a string of brainf*ck code(*.b) to the calculator stack.
+Optionally you can push to the stack another string as input.
+Execute the BFI.HP program and you can get the output string on the stack.
+
+
+## How to compile
+Install hpgcc (see [1]) and do "make bfi.hp" in src/interpreter directory.
+
+
+## Purpose
+The purpose of this work is my exercise of learning ARM programming.
+Of cource the current version is a toy and very simple implementation, but ... (see TOOD)
+
+
+## TODO
+normal and JIT compiler. (I say it a bit seriously.)
+
+
+## License
+GPL
+
+
+## Reference
+[1] Extend your 50g with C. http://sense.net/~egan/hpgcc/
+[2] BrainF*ck http://en.wikipedia.org/wiki/Brainfuck
+
+
---
+++ src/interpreter/bfi.c
@@ -0,0 +1,131 @@
+/*
+ * BFI
+ * Copyright (C) 2003 Thomas Cort
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * Program Name:  BFI
+ * Version:       1.1
+ * Date:          2003-04-29
+ * Description:   Interpreter for the Brainfuck Programming Language
+ * License:       GPL
+ * Web page:      http://www.brainfuck.ca
+ * Download:      http://www.brainfuck.ca/BFI.c
+ * Source Info:   http://www.brainfuck.ca/downloads.html
+ * Latest Ver:    http://www.brainfuck.ca/downloads.html
+ * Documentation: None
+ * Help:          tom@brainfuck.ca
+ * Developement:  tom@brainfuck.ca
+ * Bugs:          tom@brainfuck.ca
+ * Maintainer:    Thomas Cort <tom@brainfuck.ca>
+ * Developer:     Thomas Cort <tom@brainfuck.ca>
+ * Interfaces:    Command Line
+ * Source Lang:   C
+ * Build Prereq:  None
+ * Related Progs: BFIwDebug
+ * Category:      Software Development > Programming languages
+ */
+
+#include "hpgcc49.h"
+
+#define out(c) (*pout++ = c)
+#define in() (*pin++)
+
+
+int main() {
+	int pc, xc, prog_len, l = 0;
+	int x[32768];
+	char buf_out[1024*32], *pout = &buf_out;
+	char buf_in[1024*32], *pin = &buf_in;
+	char* p;
+
+	if (sat_stack_depth() == 1) {
+		p = sat_stack_pop_string_alloc();
+		prog_len = strlen(p);
+	} else {
+		strcpy(buf_in, sat_stack_pop_string_alloc());
+		strcat(buf_in, "\n");
+		p = sat_stack_pop_string_alloc();
+		prog_len = strlen(p);
+	}
+
+	sys_slowOff();
+	
+	pc = 0;
+
+	for(xc = 0; xc < 32768; xc++)
+		x[xc] = 0;
+
+	xc = 0;
+
+	for(pc = 0; pc < prog_len; pc++) {
+//		if (keyb_isAnyKeyPressed()) {
+//		break;
+//	}
+	
+		// '+'
+		if (p[pc] == 43) x[xc]++;
+
+		// '-'
+		else if (p[pc] == 45) x[xc]--;
+
+		// '.'
+		else if (p[pc] == 46) out(x[xc]);
+
+		// ','
+		else if (p[pc] == 44) x[xc] = in();
+
+		// '>'
+		else if (p[pc] == 62) xc++;
+
+		// '<'
+		else if (p[pc] == 60) xc--;
+
+		// '['
+		else if (p[pc] == 91) {
+			if (x[xc] == 0) {
+				pc++;
+				while (l > 0 || p[pc] != 93) {
+					if (p[pc] == 91) l++;
+					if (p[pc] == 93) l--;
+					pc++;
+				}
+			}
+		}
+
+		// ']'
+		else if (p[pc] == 93) {
+			pc--;
+			while (l > 0 || p[pc] != 91) {
+				if (p[pc] == 93) l++;
+				if (p[pc] == 91) l--;
+				pc--;
+			}
+			pc--;
+		}
+	}
+	
+	/*out(10);*/
+	
+	out('\0');
+	
+	sys_slowOn();
+
+	sat_stack_push_string(buf_out);
+	
+	return 0;
+}
